0d98dd4d38e59c5f50d9122f49095b9500c13c5d
John Q. Pulp <pulp@publicdomainpulp.com>
2025-12-05 13:54:46 -0600

    Simplify console output

--- PulpifierCLI/Program.cs
+++ PulpifierCLI/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Pulp.Pulpifier;
 
-Console.WriteLine("Pulpifier");
-
 string directory = Path.GetFullPath(args[0], Directory.GetCurrentDirectory());
 string rawText = File.ReadAllText(Path.Combine(directory, "book.txt"));
 string pulpText = File.ReadAllText(Path.Combine(directory, "pulp.txt"));
@@ -9,9 +7,9 @@ string pulpText = File.ReadAllText(Path.Combine(directory, "pulp.txt"));
 string html = Compiler.BuildHtml(rawText, pulpText, out Dictionary<string, int> imageFiles);
 File.WriteAllText(Path.Combine(directory, "out.html"), html);
 
-Console.WriteLine("Written.");
-
-if (args.Length == 2) {
+if (args.Length == 1) {
+	Console.WriteLine("Written.");
+} else if (args.Length == 2) {
 	int imagesTo = int.Parse(args[1]);
 	List<string> filesToPrint = imageFiles.Where(kvp => kvp.Value < imagesTo).Select(kvp => kvp.Key).ToList();
 	filesToPrint.Sort();
