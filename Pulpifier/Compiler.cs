ab91f9b85ad2f146ecc0779b10a982ac0f9b347e
John Q. Pulp <pulp@publicdomainpulp.com>
2025-12-12 10:44:01 -0600

    Make headers bigger

--- Pulpifier/Compiler.cs
+++ Pulpifier/Compiler.cs
@@ -271,7 +271,7 @@ public static class Compiler {
 							part = Regex.Replace(part, @"\*(.*?)\*", "<i>$1</i>");
 
 							part = Regex.Replace(part, @"^(#{1,6})\s+(.*)$",m => {
-								int level = m.Groups[1].Value.Length;
+								int level = Math.Min(6, m.Groups[1].Value.Length * 2 - 3);
 								string text = m.Groups[2].Value;
 								return $"<h{level}>{text}</h{level}>";
 							}, RegexOptions.Singleline);
