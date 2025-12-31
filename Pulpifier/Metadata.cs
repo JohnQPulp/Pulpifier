f3bf48d3c1cc54294490c12d9eb34bcd6d580ebc
John Q. Pulp <pulp@publicdomainpulp.com>
2025-12-31 12:41:36 -0600

    Add short title

--- Pulpifier/Metadata.cs
+++ Pulpifier/Metadata.cs
@@ -4,6 +4,7 @@ namespace Pulp.Pulpifier;
 
 public class Metadata {
 	public required string Title { get; init; }
+	public required string ShortTitle {  get; init; }
 	public required string Author { get; init; }
 	public required int Year { get; init; }
 	public required string Source { get; init; }
@@ -14,7 +15,7 @@ public class Metadata {
 
 	public static Metadata Parse(string json) {
 		Metadata metadata = JsonSerializer.Deserialize<Metadata>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-		if (metadata.Title == "" || metadata.Author == "" || metadata.Year == 0 || metadata.Source == "" || metadata.Words == 0 || metadata.Repo == "" || metadata.Links.Count == 0 || metadata.Links.Any(kvp => kvp.Key == "" || kvp.Value == "") || metadata.Blurb == "") throw new Exception("Missing required metadata.");
+		if (metadata.Title == "" || metadata.ShortTitle == "" || metadata.Author == "" || metadata.Year == 0 || metadata.Source == "" || metadata.Words == 0 || metadata.Repo == "" || metadata.Links.Count == 0 || metadata.Links.Any(kvp => kvp.Key == "" || kvp.Value == "") || metadata.Blurb == "") throw new Exception("Missing required metadata.");
 		return metadata;
 	}
 }
