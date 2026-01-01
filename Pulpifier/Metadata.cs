b24f2e01073b5536f2d1cb31ad6b97ce1e162155
John Q. Pulp <pulp@publicdomainpulp.com>
2026-01-01 08:31:57 -0600

    Add date

--- Pulpifier/Metadata.cs
+++ Pulpifier/Metadata.cs
@@ -10,6 +10,7 @@ public class Metadata {
 	public required int Year { get; init; }
 	public required string Source { get; init; }
 	public required int Words { get; init; }
+	public required DateOnly PulpDate { get; init; }
 	public required string Repo { get; init; }
 	public required Dictionary<string, string> Links { get; init; }
 	public required string Blurb { get; init; }
@@ -17,6 +18,7 @@ public class Metadata {
 	public static Metadata Parse(string json) {
 		Metadata metadata = JsonSerializer.Deserialize<Metadata>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 		if (metadata.Title == "" || metadata.ShortTitle == "" || metadata.Author == "" || metadata.Year == 0 || metadata.Source == "" || metadata.Words == 0 || metadata.Repo == "" || metadata.Links.Count == 0 || metadata.Links.Any(kvp => kvp.Key == "" || kvp.Value == "") || metadata.Blurb == "") throw new Exception("Missing required metadata.");
+		if (metadata.PulpDate < new DateOnly(2026, 1, 1)) throw new Exception("Bad date.");
 		return metadata;
 	}
 }
